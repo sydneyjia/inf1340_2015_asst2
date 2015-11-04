77f62a4320e75e6bb6db672ef8eb6b897d1cab35
odoggett <oliviatheresadoggett@gmail.com>
2015-11-04 10:35:38 -0500

    Exercise 1 complete - still need test cases

--- exercise1.py
+++ exercise1.py
@@ -11,16 +11,35 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
-
 def pig_latinify(word):
     """
-    Describe your function
-
+    Convert English word input and convert into Pig Latin word.
     :param :
-    :return:
+    :return: output_word
     :raises:
-
     """
-    result = ""
 
-    return result
+    vowels = list("aeiouyAEIOUY")
+    letter_counter = 0
+
+    if word.isalpha() == False:
+        print "Error! Please only enter characters between A-Z."
+        return False
+
+    if word[0] in vowels:
+        print(word + 'yay')
+        return True
+
+    for letter in word:
+        if letter in vowels:
+            new_word = word[letter_counter:] + word[:letter_counter] + 'ay'
+            print(new_word)
+            return True
+
+        letter_counter += 1
+
+
+word_request = True
+while word_request:
+    input = raw_input("Please enter a word:\n")
+    word_request = not pig_latinify(input)
