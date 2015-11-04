ab3560ed924c98708095af4531f3fe4fb9e5d77d
odoggett <oliviatheresadoggett@gmail.com>
2015-11-04 17:34:52 -0500

    Exercise 1 complete - still need test cases

--- test_exercise1.py
+++ test_exercise1.py
@@ -16,12 +16,26 @@ from exercise1 import pig_latinify
 
 
 def test_basic():
-    """
-    Basic test cases from assignment hand out
-    """
     assert pig_latinify("dog") == "ogday"
     assert pig_latinify("scratch") == "atchscray"
     assert pig_latinify("is") == "isyay"
     assert pig_latinify("apple") == "appleyay"
+    return
+
+def test_advanced():
+    assert pig_latinify("George") == "eorgeGay"
+    assert pig_latinify("Elly") == "Ellyay"
+    assert pig_latinify("asdfghhjkloiytFFFv") == "asdfghhjkloiytFFFvyay"
+    assert pig_latinify("phlegm") == "egmphlay"
+    return
+
+def test_errors():
+    assert pig_latinify(1234) == "Error! Please only enter characters between A-Z. Please enter a word:"
+    assert pig_latinify("Elly-May") == "Error! Please only enter characters between A-Z. Please enter a word:"
+    assert pig_latinify("Winston Churchill") == "Error! Please only enter characters between A-Z. Please enter a word:"
+    assert pig_latinify("Ke$ha") == "Error! Please only enter characters between A-Z. Please enter a word:"
+    return
+
+
 
 
